ffe034dcabed2ae903745a4ab645d6a8f9e887d5
IRSHADPM <irshadmammootty@gmail.com>
2017-02-26 20:19:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/articl-one',function(req,res){
+    res.send('article one will be served here');
+});
+app.get('/articl-tow',function(req,res){
+    res.send('article two will be served here');
+});
+app.get('/articl-three',function(req,res){
+    res.send('article three will be served here');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
